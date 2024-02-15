4ee68983c93d335e516822f91796501cfc127efd
Igor MInenko <igorymin2002@gmail.com>
2024-02-16 00:45:13 +0100

    Creating repository with new file for the game

---
+++ MyGame/MyGame.py
@@ -0,0 +1,8 @@
+import time
+import pygame
+import random
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+pygame.display.set_caption("Space Ninja")
