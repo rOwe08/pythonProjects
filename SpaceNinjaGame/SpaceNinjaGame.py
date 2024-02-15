8e325c1f017e414736c6298a5b65e30b131a59fb
Igor MInenko <igorymin2002@gmail.com>
2024-02-16 00:58:12 +0100

    Adding the main while loop

--- SpaceNinjaGame/SpaceNinjaGame.py
+++ SpaceNinjaGame/SpaceNinjaGame.py
@@ -6,3 +6,17 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 800, 600
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 pygame.display.set_caption("Space Ninja")
+
+
+def main():
+    IsRunning = True
+    print("Space Ninja Game")
+    while IsRunning:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                IsRunning = False
+                break
+    pygame.quit()
+
+if(__name__ == "__main__"):
+    main()
